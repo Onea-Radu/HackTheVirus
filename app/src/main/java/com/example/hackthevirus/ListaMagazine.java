755cd3bee2c0665d4c1c762cefef170a5e22691c
vladirares <jeleavladimir@yahoo.ro>
2020-04-16 17:27:46 +0300

    lista magazine

--- app/src/main/java/com/example/hackthevirus/ListaMagazine.java
+++ app/src/main/java/com/example/hackthevirus/ListaMagazine.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -32,6 +33,16 @@ public class ListaMagazine extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_magazine);
         final ListView listViewMagazine = (ListView) findViewById(R.id.listViewMagazine);
+
+        Button mapBtn = findViewById(R.id.mapBtn);
+
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
 
         Button loginBtn = findViewById(R.id.loginBtn);
 
